c9e58d8131946d273d71024e08822d443de8f657
OFS1\subramania.ravi <subramania.ravi@object-frontier.com>
2019-08-12 17:15:52 +0530

    Modified PersonIntegration test

--- src/test/java/com/objectfrontier/training/java/jdbc/test/PersonIntegration.java
+++ src/test/java/com/objectfrontier/training/java/jdbc/test/PersonIntegration.java
@@ -26,6 +26,7 @@ public class PersonIntegration {
     @BeforeClass
     public void startJetty() throws Exception {
 
+        System.out.println(this.getClass().getClassLoader().getResource(""));
         String webConfig = this.getClass().getClassLoader().getResource("./WEB-INF/web.xml").getFile();
         String webDirLocation = this.getClass().getClassLoader().getResource("").getFile();
         jettyServerHelper = new JettyServerHelper(8081, webConfig, webDirLocation, "/");
